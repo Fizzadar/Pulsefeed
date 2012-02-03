bafbcee6dfcc14bb76c4f8168ed72c0ef053f7c2
Nick Barrett <nick@fanaticaldev.com>
2012-02-03 18:55:44 +0000

    cron fixes

--- cron.php
+++ cron.php
@@ -1,7 +1,7 @@
 <?php
 	//invalid cron?
 	if( !defined( 'STDIN' ) or !isset( $argv[1] ) or !file_exists( __DIR__ . '/app/process/cron/' . $argv[1] . '.php' ) )
-		die( 'Invalid cron' );
+		die( 'Invalid cron: ' . __DIR__ . '/app/process/cron/' . $argv[1] . '.php' );
 	
 	//ok lets go
 	$_GET['process'] = 'cron-' . $argv[1];
