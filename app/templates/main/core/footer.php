9a2918ee1e87adde377ab2d2a296317ccb76061d
Nick Barrett <nick@fanaticaldev.com>
2012-02-21 01:05:18 +0000

    added google analytics code

--- app/templates/main/core/footer.php
+++ app/templates/main/core/footer.php
@@ -14,5 +14,20 @@
 		</div><!--end wrap-->
 	</div><!--end footer-->
 
+<!--google analytics-->
+<script type="text/javascript">
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-20248346-10']);
+_gaq.push(['_trackPageview']);
+(function() {
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+</script>
+
+
+<!-- thanks for all the fish -->
+
 </body>
 </html>
