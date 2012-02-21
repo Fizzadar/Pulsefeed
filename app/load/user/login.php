9db4acbbe2ed379e82efbf99bd72c5a49bd81888
Nick Barrett <nick@fanaticaldev.com>
2012-02-21 01:05:10 +0000

    fix redirect url on login

--- app/load/user/login.php
+++ app/load/user/login.php
@@ -2,7 +2,7 @@
 	global $mod_db, $mod_user, $mod_app, $mod_cookie;
 
 	//where are we going after logging in?
-	$mod_cookie->set( 'redirectUrl', isset( $_SERVER['HTTP_REFERER'] ) ? $_SERVER['HTTP_REFERER'] : $c_config['root'] );
+	$mod_cookie->set( 'redirectUrl', $c_config['root'] );
 
 	//start template
 	$template = new mod_template();
