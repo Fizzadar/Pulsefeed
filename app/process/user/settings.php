3edba95fd42dd043a7c62d108f01dcfa4459def1
Nick Barrett <nick@fanaticaldev.com>
2012-02-24 17:19:44 +0000

    + settings

---
+++ app/process/user/settings.php
@@ -0,0 +1,52 @@
+<?php
+	/*
+		file: app/process/user/settings.php
+		desc: save user settings
+	*/
+
+	global $mod_session, $mod_db, $mod_user, $mod_message, $mod_cookie;
+
+	$redir = $c_config['root'] . '/settings';
+
+	//token?
+	if( !isset( $_POST['mod_token'] ) or !$mod_session->validate( $_POST['mod_token'] ) ):
+		$mod_message->add( 'InvalidToken' );
+		die( header( 'Location: ' . $redir ) );
+	endif;
+
+	//check post data
+	if( !isset( $_POST['username'] ) or empty( $_POST['username'] ) or !isset( $_POST['email'] ) ):
+		$mod_message->add( 'InvalidPost' );
+		die( header( 'Location: ' . $redir ) );
+	endif;
+
+	//hmtl ents on username
+	$_POST['username'] = filter_var( $_POST['username'], FILTER_SANITIZE_URL );
+
+	//validate the email (as much as we can)
+	if( !empty( $_POST['email'] ) and !filter_var( $_POST['email'], FILTER_VALIDATE_EMAIL ) ):
+		$mod_message->add( 'InvalidEmail' );
+		die( header( 'Location: ' . $redir ) );
+	endif;
+
+	//check login
+	if( !$mod_user->check_login() ):
+		$mod_message->add( 'MustLogin' );
+		die( header( 'Location: ' . $redir ) );
+	endif;
+
+	//now, update our data
+	$mod_user->set_data( array(
+		'name' => $_POST['username'],
+		'email' => $_POST['email'],
+		'daily_email' => ( isset( $_POST['daily_email'] ) and $_POST['daily_email'] == 'on' ) ? 1 : 0,
+		'weekly_email' => ( isset( $_POST['weekly_email'] ) and $_POST['weekly_email'] == 'on' ) ? 1 : 0,
+	) );
+
+	//delete username cookie if needed
+	$mod_cookie->delete( 'ChangeUsernameMessage' );
+
+	//redirect
+	$mod_message->add( 'SettingsUpdated' );
+	header( 'Location: ' . $redir );
+?>
---
+++ app/templates/main/user/settings.php
@@ -0,0 +1,52 @@
+<?php
+	global $mod_token, $mod_user, $mod_cookie;
+?>
+
+	<div id="header">
+		<div class="wrap">
+			<div class="left">
+			</div>
+
+			<h1>Your Settings</h1>
+		</div><!--end wrap-->
+	</div><!--end header-->
+
+	<div class="wrap" id="content">
+		<div class="main">
+			<form action="<?php echo $c_config['root']; ?>/process/settings" method="post" class="left half">
+				<label for="username">Username:</label>
+				<input type="text" name="username" id="username" value="<?php echo $this->content['settings']['name']; ?>" maxlength="30" />
+
+				<label for="email">Email (optional):</label>
+				<input type="text" name="email" id="email" value="<?php echo $this->content['settings']['email']; ?>" />
+
+				<p><em>Note:</em> <strong>email updates don't currently run</strong></p>
+				<label class="checkbox" for="daily_email">Daily Email Digest:</label>
+				<input type="checkbox" name="daily_email" id="daily_email" <?php echo $this->content['settings']['daily_email'] ? 'checked' : ''; ?>/>
+
+				<label class="checkbox" for="weekly_email">Weekly Email Digest:</label>
+				<input type="checkbox" name="weekly_email" id="weekly_email" <?php echo $this->content['settings']['weekly_email'] ? 'checked' : ''; ?>/>
+
+				<input type="submit" value="Update Settings &#187;" />
+				<input type="hidden" name="mod_token" value="<?php echo $mod_token; ?>" />
+			</form>
+
+			<div class="right half">
+				<h2>Linked Accounts</h2>
+				<p>All the accounts you have used (and can use) to login to Pulsefeed. <a href="<?php echo $c_config['root']; ?>/login"><strong>Add another account &rarr;</strong></a></p>
+				<?php foreach( $this->get( 'oauths' ) as $oauth ): ?>
+					<strong><?php echo $oauth['provider']; ?></strong> &rarr; <?php echo $oauth['o_id']; ?><br />
+				<?php endforeach; ?>
+				<?php foreach( $this->get( 'oids' ) as $oid ): $bits = parse_url( $oid['open_id'] ); ?>
+					<strong><?php echo $bits['host']; ?></strong> &rarr; <?php echo $bits['path']; ?><br />
+				<?php endforeach; ?>
+			</div>
+		</div><!--end main-->
+	</div><!--end wrap-->
+
+	<div id="sidebars">
+		<div class="wrap">
+			<div class="left">
+			</div><!--end left-->
+		</div><!--end wrap-->
+	</div><!--end sidebars-->
