9525e92a231b21fc6609a9693fa4e9534b625886
Nick Barrett <nick@fanaticaldev.com>
2012-03-11 02:31:24 +0000

    read_stream fb permission

--- app/process/user/fb_out.php
+++ app/process/user/fb_out.php
@@ -6,7 +6,7 @@
 	$return_url = $c_config['root'] . '/process/login-facebook';
 
 	//and go
-	if( $url = $mod_user->fb_out( $return_url, 'publish_actions,offline_access' ) ):
+	if( $url = $mod_user->fb_out( $return_url, 'publish_actions,offline_access,read_stream' ) ):
 		header( 'Location: ' . $url );
 	else:
 		$mod_message->add( 'LoginServerError' );
