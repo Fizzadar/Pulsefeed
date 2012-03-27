efc403e94fc7e433fe6e1db8d1c25e5311e994c2
Nick Barrett <nick@fanaticaldev.com>
2012-03-28 00:52:57 +0100

    better login redirect

--- app/load/user/login.php
+++ app/load/user/login.php
@@ -5,9 +5,15 @@
 	$mod_template = new mod_template();
 
 	//where are we going after logging in?
-	$url = parse_url( $_SERVER['HTTP_REFERER'] );
-	$pfurl = parse_url( $c_config['root'] );
-	$redir = $url['host'] == $pfurl['host'] ? $_SERVER['HTTP_REFERER'] : $c_config['root'];
+	if( isset( $_SERVER['HTTP_REFERER'] ) ):
+		$url = parse_url( $_SERVER['HTTP_REFERER'] );
+		$pfurl = parse_url( $c_config['root'] );
+		$redir = $url['host'] == $pfurl['host'] ? $_SERVER['HTTP_REFERER'] : $c_config['root'];
+	else:
+		$redir = $c_config['root'];
+	endif;
+
+	//set redirect dir
 	$mod_cookie->set( 'redirectUrl', $redir );
 	$mod_template->add( 'redir', $redir );
 
